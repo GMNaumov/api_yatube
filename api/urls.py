9802b52763b07ccbc3cec62f14fed0040877754a
Georgii Naumov <gmnaumov@yandex.ru>
2020-04-10 21:05:07 +0300

    HW_API build 1.0.1

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import PostViewSet, CommentViewSet
 
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
+v1_router = DefaultRouter()
+v1_router.register('v1/posts', PostViewSet)
+v1_router.register(r'v1/posts/(?P<post_id>\d+)/comments', CommentViewSet, 'post_id')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token),
+    path('', include(v1_router.urls)),
+    path('v1/api-token-auth/', obtain_auth_token),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsOwnerOrReadOnly
@@ -13,23 +12,18 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
-    def create(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
 
     def get_queryset(self):
-        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         return Comment.objects.filter(post=post)
